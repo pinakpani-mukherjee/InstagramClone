183d7a29183d8d1069c4e4f96153b9f61f917332
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-09-15 12:37:16 +0530

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Pinakpani Mukherjee on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
